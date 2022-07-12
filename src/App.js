9069fea17c4ad5bbb304822e64de8dd86b5fa975
Rafael Wendt de Moura <rafael.wendt.moura@gmail.com>
2022-07-12 15:08:30 -0300

    Primeira versão da limpeza de cache de url

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import './App.css';
+import React, { useState } from 'react';
+
+function App() {
+
+  const [metadata, setMetadata] = useState('');
+  const [metadataparcial, setMetadataParcial] = useState('');
+  const [route, setRoute] = useState('');
+  const [routeparcial, setRouteParcial] = useState('');
+
+  const listenerInputMetada = event => {
+    const { value } = event.target;
+    setMetadata(value);
+}
+
+  const listenerInputRoute = event => {
+  const { value } = event.target;
+  setRoute(value);
+}
+
+const Gerar = () => {
+  var findMetaData = metadata.match("pathname")
+  var findRoute = route.match("pathname")
+  setMetadataParcial(metadata.slice(0, findMetaData.index-1))
+  setRouteParcial(route.slice(0, findRoute.index-1))
+}
+
+const Resultado = () => {
+  return(
+    <div className='resultado'>
+      {metadata}<br/>
+      {route}<br/>
+      {metadataparcial}<br/>
+      {routeparcial}
+    </div>
+  );
+}
+
+  return (
+    <div className="App">
+      <h2>LIMPEZA DE CACHE DE URL</h2>
+      <div className='Campos'><input onChange={listenerInputMetada} placeholder='Insira a URL inteira do Metadata'></input></div>
+      <div className='Campos'><input onChange={listenerInputRoute} placeholder='Insira a URL inteira do Route'></input></div>
+      <button onClick={Gerar}>Gerar</button>
+      <Resultado></Resultado>
+    </div>
+  );
+}
+
+export default App;
