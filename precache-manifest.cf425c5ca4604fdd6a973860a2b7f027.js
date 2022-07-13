3b0f0a5562f29c09815e7aa8f29cf292977e4b46
Rafael Wendt de Moura <rafael.wendt.moura@gmail.com>
2022-07-12 22:52:43 -0300

    Updates

---
+++ precache-manifest.cf425c5ca4604fdd6a973860a2b7f027.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e33ac06dd716b9fbe982",
+    "url": "/cache_url/static/css/main.56651bac.chunk.css"
+  },
+  {
+    "revision": "e33ac06dd716b9fbe982",
+    "url": "/cache_url/static/js/main.e33ac06d.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/cache_url/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/cache_url/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "0dd94ea57d86d35cc06a",
+    "url": "/cache_url/static/js/runtime~main.0dd94ea5.js"
+  },
+  {
+    "revision": "459a0355f5d952f77b27525f93d4f2a4",
+    "url": "/cache_url/index.html"
+  }
+];
